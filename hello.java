a1861fa0089eca1989a4b4b9001dbfd8b414cafd
Eric Schles <ericschles@gmail.com>
2014-05-07 14:56:11 -0700

    Revert "wrong on purpose"
    This reverts commit bb08d6fb76af68d84e74515ec4c9c02481369c11.

--- hello.java
+++ hello.java
@@ -2,7 +2,6 @@ public class hello{
 
     public static void main(String[] args){
 
-	Syste.out.println("Hello world");
-	System.out.println("whoops");
+	System.out.println("Hello world");
     }
 }
